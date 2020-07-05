f1038c321edfcfee2ca1bf85cb693e8be4628943
Nikita <62699820+nikitos1055@users.noreply.github.com>
2020-07-05 23:32:53 +0300

    done

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -1,9 +1,8 @@
 package com.company;
 
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class Employee implements Serializable {
 
@@ -12,6 +11,8 @@ public class Employee implements Serializable {
     private Integer id;
 
     private transient Integer salary;
+
+    File file = new File("text.txt");
 
     public Employee(String name, Integer id, Integer salary) {
         this.name = name;
@@ -29,56 +30,23 @@ public class Employee implements Serializable {
                 '}'+"\n";
     }
 
-
-
-    void serialize(Object o) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("text.txt",true));
-        if(o == null){
-            System.out.println("Nothing in class");
-        }else{
-            objectOutputStream.writeObject(o.toString());
-            System.out.print(o.toString());
+    void serialize(List<Employee> arrayList) throws IOException {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file));
+        for(Employee employee : arrayList) {
+            objectOutputStream.writeObject(employee);
         }
+            objectOutputStream.close();
     }
 
-     void deserialize(Employee[] employees) throws IOException, ClassNotFoundException {
-            File file = new File("text.txt");
-            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
-
-                employees = (Employee[]) objectInputStream.readObject();
-                System.out.print(employees);
-            
-            objectInputStream.close();
-
-
-    }
-
-
-
-    //getters & setters
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
+     void deserialize(List<Employee> arrayList) throws IOException, ClassNotFoundException {
+         ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
+         for(Employee employee : arrayList) {
+             Employee employee1 = (Employee) objectInputStream.readObject();
+             System.out.print(employee1);
+         }
+         objectInputStream.close();
+     }
 }
+
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,25 @@
 package com.company;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Employee[] employee = new Employee[5];
-        employee[0] = new Employee("Nikita" , 23213 , 2000);
-        employee[1] = new Employee("Andriy" , 12342 , 1378);
-        employee[2] = new Employee("Valyl" , 45634 , 1212);
-        employee[3] = new Employee("Myroslav" , 12452 , 1243);
-        employee[4] = new Employee("Petro" , 68746 , 1542);
+        ArrayList<Employee> employee = new ArrayList<>();
+        employee.add(new Employee("Nikita" , 23213 , 2000));
+        employee.add(new Employee("Andriy" , 12342 , 1378));
+        employee.add(new Employee("Valyl" , 45634 , 1212));
+        employee.add(new Employee("Myroslav" , 12452 , 1243));
+        employee.add(new Employee("Petro" , 68746 , 1542));
 
-        System.out.println("\nSerialize successful done.");
+        employee.get(0).serialize(employee);
 
-        for(Employee employees : employee) {
-            employees.serialize(employees);
-        }
 
         System.out.println("\nDeserialize successful done.");
+        employee.get(0).deserialize(employee);
 
-        for(Employee employees : employee) {
-            employees.deserialize(employee);
-        }
+
 
     }
 }
